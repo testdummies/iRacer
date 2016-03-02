571a579cd24abca04d21c8ac626383ddc8bad31a
iluzek <iluzek@users.noreply.github.com>
2016-03-02 14:51:43 +0000

    Changes to code
    Removed old commented out code. Fixed few indents, fixed print("something's wrong with {0}, Exception is {1}:".format(mac_address, e)) and changed true/false connections

--- SendingCommands.py
+++ SendingCommands.py
@@ -2,33 +2,27 @@ import socket
 import time
 
 class BluetoothCar:
-    #def __init__(self, mac_address="00:12:05:11:97:90"):
-    '''
-    def __init__(self, mac_address="00:12:05:09:97:76"):
-        self.socket = socket.socket(
-            socket.AF_BLUETOOTH,
-            socket.SOCK_STREAM,
-            socket.BTPROTO_RFCOMM)
+    def connectToBluetooth(self):
+        mac_address="00:12:05:11:97:90"
+        connect = False
 
-        self.socket.connect((mac_address, 1))
-    '''
-        
-    def connectToBluetooth(self, mac_address="00:12:05:09:97:76"):
         self.socket = socket.socket(
             socket.AF_BLUETOOTH,
             socket.SOCK_STREAM,
             socket.BTPROTO_RFCOMM)
 
         try:
-            # test the connection to the car and catch any errors 
+            # test the connection to the car and catch any errors
             # if the connection succeeds then we return a bool
             self.socket.connect((mac_address, 1))
             print 'connected to car'
-            return true
+            connect = True
         except Exception as e:
             # display the error and return false to say connection failed
-            print("something's wrong with {0}, Exception is {1}: .format(mac_address, e)
-            return false
+            print("something's wrong with {0}, Exception is {1}:".format(mac_address, e))
+            connect = False
+
+        return connect
 
     def drive(self,duration):
         self.socket.send('\x16')
@@ -36,13 +30,10 @@ class BluetoothCar:
 
 if __name__ == "__main__":
     car = BluetoothCar()
-    car.connectToBluetooth()
-    
     # if the connection succeeds then we can run car commands
-        if car.connectToBluetooth():
-            while True:
-                car.drive(0.05)
-        # otherwise we say not connected
-        else:
-            print 'not connected'
-        
+    if car.connectToBluetooth():
+        while True:
+            car.drive(0.05)
+    # otherwise we say not connected
+    else:
+        print 'not connected'
