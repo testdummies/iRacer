1fa2abd6dace8e4823f7d38bb75b63508a382aa5
testdummies <testdummies1026@gmail.com>
2016-03-10 12:19:50 +0000

    Bluetooth_search_method.py
    we could use this to go over the list of addresses, and pass one at a time to the lookup_name method(). it compares to see if it matches with the desired device name. if it does, it breaks out the loop and prints target_address.

---
+++ Bluetooth_search_method.py
@@ -0,0 +1,18 @@
+import bluetooth
+
+
+
+target_device = "TestCar"
+target_address = None
+
+discovered_devices = bluetooth.discover_devices()
+
+for bdaddr in discovered_devices:
+    if target_name == bluetooth.lookup_name( bdaddr ):
+        target_address = bdaddr
+        break
+
+if target_address is not None:
+    print "found target bluetooth device with address ", target_address
+else:
+    print "could not find target bluetooth device nearby"
