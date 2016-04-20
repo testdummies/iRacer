4665edaa8b0c7136a9d58a3b4b0707b04723fbb3
iluzek <iluzek@hotmail.co.uk>
2016-04-20 04:03:05 +0100

    updated for testing some manouvres

--- iRacer.py
+++ iRacer.py
@@ -1,10 +1,12 @@
 import time
-import modules.bluetooth.connection as bt_connection
+import modules.bluetooth_controls.connection as bt_connection
+import modules.movement.manoeuvres as mv
 
 
 bt_connection.set_up_device("00:12:05:11:97:90",1)
 bt_connection.set_up_bluetooth_socket()
 bt_connection.connect_bluetooth()
-bt_connection.send_command('\x80')
-time.sleep(2)
+mv.parallel_parking_left_side()
+#bt_connection.send_command('\x80')
+#time.sleep(2)
 bt_connection.disconnect_bluetooth()
