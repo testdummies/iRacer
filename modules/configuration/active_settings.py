8d456f556dd990c0784914c4e508855dbd465427
iluzek <iluzek@hotmail.co.uk>
2016-04-20 15:15:22 +0100

    config methods added and config file structured.

--- modules/configuration/active_settings.py
+++ modules/configuration/active_settings.py
@@ -4,16 +4,126 @@
 #TODO MAKE SURE SETTINGS WORK WITH SOMETHING THAT INTERPRETS KEYS
 
 import ConfigParser
-
 config = ConfigParser.ConfigParser()
-config.read('config.ini')
-print(config.sections())
 
-'''
-#[DEVICE SETTINGS]
-MAC_ADDRESS = config['DEVICE SETTINGS']['MAC_ADDRESS']
-BLUETOOTH_CHANNEL = config['DEVICE SETTINGS']['BLUETOOTH_CHANNEL']
-'''
+# [DEVICE SETTINGS]
+MAC_ADDRESS = ""
+BLUETOOTH_CHANNEL = ""
 
-print (config['DEVICE_SETTINGS']['MAC_ADDRESS'])
-print (config['DRIVE_SETTINGS']['BLUETOOTH_CHANNEL'])
+# [DRIVE_SETTINGS]
+Transmission = ""
+
+# [KEYBOARD_MAPPING]
+KEYBOARD_Steer_Left = ""
+KEYBOARD_Steer_Right = ""
+KEYBOARD_Accelerate = ""
+KEYBOARD_Break_Reverse = ""
+KEYBOARD_Hand_Break = ""
+KEYBOARD_NOS = ""
+KEYBOARD_Gear_Up = ""
+KEYBOARD_Gear_Down = ""
+KEYBOARD_Cruise_Control = ""
+KEYBOARD_Record_Movements_On = ""
+KEYBOARD_Record_Movements_Off = ""
+
+# [CONTROLLER_MAPPING]
+CONTROLLER_Steer_Left = ""
+CONTROLLER_Steer_Right = ""
+CONTROLLER_Accelerate = ""
+CONTROLLER_Break_Reverse = ""
+CONTROLLER_Hand_Break = ""
+CONTROLLER_NOS = ""
+CONTROLLER_Gear_Up = ""
+CONTROLLER_Gear_Down = ""
+CONTROLLER_Cruise_Control = ""
+CONTROLLER_Record_Movements_On = ""
+CONTROLLER_Record_Movements_Off = ""
+
+
+def load_config():
+    global MAC_ADDRESS, BLUETOOTH_CHANNEL, Transmission, KEYBOARD_Steer_Left, KEYBOARD_Steer_Right
+    global KEYBOARD_Accelerate, KEYBOARD_Break_Reverse, KEYBOARD_Hand_Break, KEYBOARD_NOS, KEYBOARD_Gear_Up
+    global KEYBOARD_Gear_Down, KEYBOARD_Cruise_Control, KEYBOARD_Record_Movements_On, KEYBOARD_Record_Movements_Off
+    global CONTROLLER_Steer_Left, CONTROLLER_Steer_Right, CONTROLLER_Accelerate, CONTROLLER_Break_Reverse, CONTROLLER_Hand_Break
+    global CONTROLLER_NOS, CONTROLLER_Gear_Up, CONTROLLER_Gear_Down, CONTROLLER_Cruise_Control, CONTROLLER_Record_Movements_On
+    global CONTROLLER_Record_Movements_On, CONTROLLER_Record_Movements_Off
+
+    config.read('config.ini')
+    #[DEVICE SETTINGS]
+    MAC_ADDRESS                     = config.get('DEVICE_SETTINGS', 'MAC_ADDRESS')
+    BLUETOOTH_CHANNEL               = config.get('DEVICE_SETTINGS', 'BLUETOOTH_CHANNEL')
+
+    #[DRIVE_SETTINGS]
+    Transmission                    = config.get('DRIVE_SETTINGS', 'Transmission')
+
+    #[KEYBOARD_MAPPING]
+    KEYBOARD_Steer_Left             = config.get('KEYBOARD_MAPPING', 'Steer Left')
+    KEYBOARD_Steer_Right            = config.get('KEYBOARD_MAPPING', 'Steer Right')
+    KEYBOARD_Accelerate             = config.get('KEYBOARD_MAPPING', 'Accelerate')
+    KEYBOARD_Break_Reverse          = config.get('KEYBOARD_MAPPING', 'Break/Reverse')
+    KEYBOARD_Hand_Break             = config.get('KEYBOARD_MAPPING', 'Hand Break')
+    KEYBOARD_NOS                    = config.get('KEYBOARD_MAPPING', 'N.O.S')
+    KEYBOARD_Gear_Up                = config.get('KEYBOARD_MAPPING', 'Gear Up')
+    KEYBOARD_Gear_Down              = config.get('KEYBOARD_MAPPING', 'Gear Down')
+    KEYBOARD_Cruise_Control         = config.get('KEYBOARD_MAPPING', 'Cruise Control')
+    KEYBOARD_Record_Movements_On    = config.get('KEYBOARD_MAPPING', 'Record Movements On')
+    KEYBOARD_Record_Movements_Off   = config.get('KEYBOARD_MAPPING', 'Record Movements Off')
+
+    #[CONTROLLER_MAPPING]
+    CONTROLLER_Steer_Left             = config.get('CONTROLLER_MAPPING', 'Steer Left')
+    CONTROLLER_Steer_Right            = config.get('CONTROLLER_MAPPING', 'Steer Right')
+    CONTROLLER_Accelerate             = config.get('CONTROLLER_MAPPING', 'Accelerate')
+    CONTROLLER_Break_Reverse          = config.get('CONTROLLER_MAPPING', 'Break/Reverse')
+    CONTROLLER_Hand_Break             = config.get('CONTROLLER_MAPPING', 'Hand Break')
+    CONTROLLER_NOS                    = config.get('CONTROLLER_MAPPING', 'N.O.S')
+    CONTROLLER_Gear_Up                = config.get('CONTROLLER_MAPPING', 'Gear Up')
+    CONTROLLER_Gear_Down              = config.get('CONTROLLER_MAPPING', 'Gear Down')
+    CONTROLLER_Cruise_Control         = config.get('CONTROLLER_MAPPING', 'Cruise Control')
+    CONTROLLER_Record_Movements_On    = config.get('CONTROLLER_MAPPING', 'Record Movements On')
+    CONTROLLER_Record_Movements_Off   = config.get('CONTROLLER_MAPPING', 'Record Movements Off')
+
+def print_entire_config():
+    # [DEVICE SETTINGS]
+    print (MAC_ADDRESS)
+    print (BLUETOOTH_CHANNEL)
+
+    # [DRIVE_SETTINGS]
+    print (Transmission)
+
+    # [print (KEYBOARD_MAPPING]
+    print (KEYBOARD_Steer_Left)
+    print (KEYBOARD_Steer_Right)
+    print (KEYBOARD_Accelerate)
+    print (KEYBOARD_Break_Reverse)
+    print (KEYBOARD_Hand_Break)
+    print (KEYBOARD_NOS)
+    print (KEYBOARD_Gear_Up)
+    print (KEYBOARD_Gear_Down)
+    print (KEYBOARD_Cruise_Control)
+    print (KEYBOARD_Record_Movements_On)
+    print (KEYBOARD_Record_Movements_Off)
+
+    # [print (CONTROLLER_MAPPING]
+    print (CONTROLLER_Steer_Left)
+    print (CONTROLLER_Steer_Right)
+    print (CONTROLLER_Accelerate)
+    print (CONTROLLER_Break_Reverse)
+    print (CONTROLLER_Hand_Break)
+    print (CONTROLLER_NOS)
+    print (CONTROLLER_Gear_Up)
+    print (CONTROLLER_Gear_Down)
+    print (CONTROLLER_Cruise_Control)
+    print (CONTROLLER_Record_Movements_On)
+    print (CONTROLLER_Record_Movements_Off)
+
+
+def update_config_field(section, option, value):
+    config.set(section,option,value)
+    with open('config.ini', 'w') as configfile:
+        config.write(configfile)
+    load_config()
+
+
+load_config()
+update_config_field('CONTROLLER_MAPPING',  'Record Movements Off', 'TEST2')
+print_entire_config()
