7b8d8188bbfdecb279369cf23410817f100a00ae
rossi17 <ross170696@hotmail.com>
2016-04-22 09:18:16 +0100

    Added files via upload
    updates

--- modules/movement/manoeuvres.py
+++ modules/movement/manoeuvres.py
@@ -2,6 +2,7 @@ import modules.calculations.math as math
 import modules.bluetooth_controls.connection as bt
 
 
+
 # TODO CHECK IF GEAR 1 which is 1 lower than speed 2 is working correctly.
 # Speed will always be 2 due to using time to maneuver
 # Increasing speed will throw off out calibrates movements
@@ -23,8 +24,8 @@ def execute_manoeuvre(time_list, gear_list, direction_list):
     for i in range(len(time_list)):
         command = math.get_combined_value(direction_list[i], gear_list[i])
         bt.send_command(command, time_list[i])
-    command = math.get_combined_value(0,  0)
-    bt.send_command(command, time_list[0.01])
+    command = math.get_combined_value("STOP",  0)
+    bt.send_command(command, 0.01)
 
 
 # =============================================================================
@@ -32,7 +33,7 @@ def execute_manoeuvre(time_list, gear_list, direction_list):
 # Description:  TODO description
 # ==============================================================================
 def three_point_turn_right():
-    sq_time = [2, 2.4, 1, 1.6, 1, 1.48]
+    sq_time = [2, 2.4, 1, 1.6, 1, 1]
     sq_gear = [1, 1, 1, 1, 1, 1]
     sq_direction = ["STOP", "FR", "STOP", "RL", "STOP", "FR"]
     execute_manoeuvre(sq_time, sq_gear, sq_direction)
@@ -44,9 +45,9 @@ def three_point_turn_right():
 #                Should end up facing 180 degrees on oposite side of the "Road"
 # ==============================================================================
 def three_point_turn_left():
-    sq_time = [2, 2, 1, 2, 1, 1.4]
-    sq_gear = [1, 1, 1, 1, 1, 1]
-    sq_direction = ["STOP", "FL", "STOP", "RR", "STOP", "FL"]
+    sq_time = [2, 2, 1, 2, 1, 0.8, 0.5]#0.4
+    sq_gear = [1, 1, 1, 1, 1, 1,1]
+    sq_direction = ["STOP", "FL", "STOP", "RR", "STOP", "FL", "FS"]
     execute_manoeuvre(sq_time, sq_gear, sq_direction)
 
 
@@ -56,7 +57,7 @@ def three_point_turn_left():
 # ==============================================================================
 def parallel_parking_right_side():
     # Combined all settings to sequence lists
-    sq_time = [2, 1.3, 1, 1.1, 1, 0.6, 1, 0.3]  # sequence time etc.
+    sq_time = [2, 1.3, 1, 0.8, 1, 0.8, 1, 0.3]  # sequence time etc.
     sq_gear = [1, 1, 1, 1, 1, 1, 1, 1]
     sq_direction = ["STOP", "RR", "STOP", "RL", "STOP", "FR", "STOP", "FS"]
     execute_manoeuvre(sq_time, sq_gear, sq_direction)
@@ -67,7 +68,7 @@ def parallel_parking_right_side():
 # Description:  parallel park to left hand side using 3 turning movements to achieve it
 # ==============================================================================
 def parallel_parking_left_side():
-    sq_time = [2, 1.3, 1, 0.8, 1, 0.8, 1, 0.1]
+    sq_time = [2, 1.3, 1, 0.8, 1, 0.6, 1, 0.4]
     sq_gear = [1, 1, 1, 1, 1, 1, 1, 1]
     sq_direction = ["STOP", "RL", "STOP", "RR", "STOP", "FL", "STOP", "FS"]
     execute_manoeuvre(sq_time, sq_gear, sq_direction)
@@ -78,7 +79,7 @@ def parallel_parking_left_side():
 # Description:  180 turn going to left with 1 long movement
 # ==============================================================================
 def turn_around_left():
-    sq_time = [2, 5]
+    sq_time = [2, 4.7]
     sq_gear = [1, 1]
     sq_direction = ["STOP", "FL"]
     execute_manoeuvre(sq_time, sq_gear, sq_direction)
@@ -89,7 +90,7 @@ def turn_around_left():
 # Description:  180 turn going to right with 1 long movement
 # ==============================================================================
 def turn_around_right():
-    sq_time = [2, 5]
+    sq_time = [2, 4.5]
     sq_gear = [1, 1]
     sq_direction = ["STOP", "FR"]
     execute_manoeuvre(sq_time, sq_gear, sq_direction)
@@ -100,7 +101,7 @@ def turn_around_right():
 # Description:  360 turn going to right with 1 long movement
 # ==============================================================================
 def circle_right():
-    sq_time = [2, 9]
+    sq_time = [2, 8.5]
     sq_gear = [1, 1]
     sq_direction = ["STOP", "FR"]
     execute_manoeuvre(sq_time, sq_gear, sq_direction)
